f93f32658f1eb786163195a9ddcff79ec9d9f3e3
Victoria Li <vickitori109@gmail.com>
2025-01-23 22:15:32 -0800

    adding game logic

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
+
+function renderState(): void {
+    
+}
+
+
 function startGame(): void {
     document.getElementById("start-container")!.style.display = 'none';
     document.getElementById("game-container")!.style.display = 'block';
