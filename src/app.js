65b512ea4ae30fa1c2cfb857dc40f8098e3a76d1
Victoria Li <vickitori109@gmail.com>
2025-01-26 01:55:36 -0800

    html, css refactor - test run of switching logic

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
+function renderState() {
+    var choicesContainer = document.getElementById('game-choices');
+    var mainImg = document.getElementById('game-img');
+    mainImg.src = "assets/test-game.png";
+    var button = document.createElement('button');
+    button.textContent = "test";
+    button.className = "game-choice-button";
+    var button2 = document.createElement('button');
+    button2.textContent = "tblalblblblblbllblbl";
+    button2.className = "game-choice-button";
+    var button3 = document.createElement('button');
+    button3.textContent = "tblalblb222";
+    button3.className = "game-choice-button";
+    choicesContainer.appendChild(button);
+    choicesContainer.appendChild(button2);
+    choicesContainer.appendChild(button3);
+}
 function startGame() {
-    document.getElementById("start-container").style.display = 'none';
-    document.getElementById("game-container").style.display = 'block';
+    //document.getElementById('start-container')!.style.display = 'none';
+    //document.getElementById('game-container')!.style.display = 'flex';
+    document.getElementById('start-button').style.display = 'none';
+    renderState();
 }
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 
 function renderState(): void {
-    
+    const choicesContainer = document.getElementById('game-choices');
+
+    const mainImg = document.getElementById('game-img') as HTMLImageElement;
+    mainImg.src = "assets/test-game.png"
+
+    const button = document.createElement('button');
+    button.textContent = "test"; 
+    button.className = "game-choice-button";
+
+    const button2 = document.createElement('button');
+    button2.textContent = "tblalblblblblbllblbl"; 
+    button2.className = "game-choice-button";
+
+    const button3 = document.createElement('button');
+    button3.textContent = "tblalblb222"; 
+    button3.className = "game-choice-button";
+
+    choicesContainer!.appendChild(button);
+    choicesContainer!.appendChild(button2);
+    choicesContainer!.appendChild(button3);
 }
 
-
 function startGame(): void {
-    document.getElementById("start-container")!.style.display = 'none';
-    document.getElementById("game-container")!.style.display = 'block';
+    //document.getElementById('start-container')!.style.display = 'none';
+    //document.getElementById('game-container')!.style.display = 'flex';
+    document.getElementById('start-button')!.style.display = 'none'; 
+    renderState(); 
 }
