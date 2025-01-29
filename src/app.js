b1ae04d1cc1a1384d5760435f4b849d542fa51a6
Victoria Li <vickitori109@gmail.com>
2025-01-28 23:30:23 -0800

    added endings

--- src/app.js
+++ src/app.js
@@ -1,23 +1,39 @@
-function renderState() {
-    var choicesContainer = document.getElementById('game-choices');
-    var mainImg = document.getElementById('game-img');
-    mainImg.src = "assets/test-game.png";
-    var button = document.createElement('button');
-    button.textContent = "test";
-    button.className = "game-choice-button";
-    var button2 = document.createElement('button');
-    button2.textContent = "tblalblblblblbllblbl";
-    button2.className = "game-choice-button";
-    var button3 = document.createElement('button');
-    button3.textContent = "tblalblb222";
-    button3.className = "game-choice-button";
-    choicesContainer.appendChild(button);
-    choicesContainer.appendChild(button2);
-    choicesContainer.appendChild(button3);
+var story = {};
+var endings = {
+    "adventure": 0,
+    "romance": 0,
+    "betrayal": 0,
+    "romance-pt-2": 0,
+    "secret ending": 0,
+    "villain-wins": 0,
+    "100-percent": 0,
+};
+var state = 0;
+function renderState(state) {
+    // Select Relevant HTML Elements
+    var gameContainer = document.getElementById('game-choices');
+    var gameImage = document.getElementById('game-img');
+    // const mainImg = document.getElementById('game-img') as HTMLImageElement;
+    // mainImg.src = "assets/test-game.png"
+    // const button = document.createElement('button');
+    // button.textContent = "test"; 
+    // button.className = "game-choice-button";
+    // const button2 = document.createElement('button');
+    // button2.textContent = "tblalblblblblbllblbl"; 
+    // button2.className = "game-choice-button";
+    // const button3 = document.createElement('button');
+    // button3.textContent = "tblalblb222"; 
+    // button3.className = "game-choice-button";
+    // choicesContainer!.appendChild(button);
+    // choicesContainer!.appendChild(button2);
+    // choicesContainer!.appendChild(button3);
 }
 function startGame() {
     //document.getElementById('start-container')!.style.display = 'none';
     //document.getElementById('game-container')!.style.display = 'flex';
     document.getElementById('start-button').style.display = 'none';
-    renderState();
+    renderState(state);
 }
+window.onload = function () {
+    renderState(state);
+};
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,51 @@
+const story = {
 
-function renderState(): void {
-    const choicesContainer = document.getElementById('game-choices');
+}
 
-    const mainImg = document.getElementById('game-img') as HTMLImageElement;
-    mainImg.src = "assets/test-game.png"
+const endings = {
+    "adventure" : 0,
+    "romance" : 0,
+    "betrayal" : 0,
+    "romance-pt-2" : 0,
+    "secret ending" : 0,
+    "villain-wins" : 0, 
+    "100-percent" : 0,
+}
 
-    const button = document.createElement('button');
-    button.textContent = "test"; 
-    button.className = "game-choice-button";
+let state = 0;
 
-    const button2 = document.createElement('button');
-    button2.textContent = "tblalblblblblbllblbl"; 
-    button2.className = "game-choice-button";
+function renderState(state: number): void {
+    // Select Relevant HTML Elements
+    const gameContainer = document.getElementById('game-choices');
+    const gameImage = document.getElementById('game-img') as HTMLImageElement; 
 
-    const button3 = document.createElement('button');
-    button3.textContent = "tblalblb222"; 
-    button3.className = "game-choice-button";
+    // const mainImg = document.getElementById('game-img') as HTMLImageElement;
+    // mainImg.src = "assets/test-game.png"
 
-    choicesContainer!.appendChild(button);
-    choicesContainer!.appendChild(button2);
-    choicesContainer!.appendChild(button3);
+    // const button = document.createElement('button');
+    // button.textContent = "test"; 
+    // button.className = "game-choice-button";
+
+    // const button2 = document.createElement('button');
+    // button2.textContent = "tblalblblblblbllblbl"; 
+    // button2.className = "game-choice-button";
+
+    // const button3 = document.createElement('button');
+    // button3.textContent = "tblalblb222"; 
+    // button3.className = "game-choice-button";
+
+    // choicesContainer!.appendChild(button);
+    // choicesContainer!.appendChild(button2);
+    // choicesContainer!.appendChild(button3);
 }
 
 function startGame(): void {
     //document.getElementById('start-container')!.style.display = 'none';
     //document.getElementById('game-container')!.style.display = 'flex';
     document.getElementById('start-button')!.style.display = 'none'; 
-    renderState(); 
+    renderState(state); 
+}
+
+window.onload = () => {
+    renderState(state);
 }
